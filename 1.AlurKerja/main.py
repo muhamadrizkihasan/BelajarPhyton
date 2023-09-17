fc205423977f9c3224476d89af2d0c87abeb0239
Muhamad Rizki Hasan <muhamadrizkihasan@smkwikrama.sch.id>
2023-09-17 20:44:06 +0700

    Menambahkan Folder AlurKerjaPython

---
+++ 1.AlurKerja/main.py
@@ -0,0 +1,17 @@
+import time
+start_time = time.time()
+
+print("Hello")
+print("World")
+print("Heloo World")
+
+print("Halo Chantiek")
+# Ini adalah comment 
+a = 10 # Ini adalah comment juga
+"""Ada apa dengan ucup dan 
+otong si ganteng
+dalam comment multiline"""
+print(a)
+print(time.time() - start_time, "Detik")
+# Kita bisa mengcompile python ke yang namanya bytecode
+# Cara mengcompile, buka terminal dan tuliska python -m py_compile Main.py
