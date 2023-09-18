ee13005bef3e6ad19f98f997c48e16b75bedbf7c
Muhamad Rizki Hasan <muhamadrizkihasan@smkwikrama.sch.id>
2023-09-18 21:29:34 +0700

    Menambahkan Folder Variable

---
+++ 2.Variable/main.py
@@ -0,0 +1,25 @@
+# Variable adalah tempat menyimpan data
+
+# Menaruh / assigment nilai
+a = 10
+x = 5
+panjang = 1000
+
+# Pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# Penamaan
+nilai_y = 15 # Dengan menggunakan underscord boleh
+juta10 = 10000000 # Ini boleh
+nilaiZ = 17.5 # Ini boleh
+
+# Pemanggilan kedua
+print("Nilai a = ", a)
+a = 7 
+print("Nilai a = ", a)
+
+# Assigment indirect
+b = a
+print("Nilai b = ", b)
