0e02df6aa6dc0f6707dcb96d685315eebcf7426e
Muhamad Rizki Hasan <muhamadrizkihasan@smkwikrama.sch.id>
2023-09-26 15:05:28 +0700

    Menambahkan folder aritmatika

---
+++ 7.LatihanAritmatika/main.py
@@ -0,0 +1,21 @@
+# Latihan konversi satuan temperature
+
+# Program konversi celcius ke satuan lain
+
+print("\nPROGRAM KONVERSI TEMPERATUR\N")
+
+celcius = float(input("Masukan suhu dalam celcius :"))
+print("Suhu adalah", celcius, "Celcius")
+
+# Reamur
+reamur = (4/5) * celcius
+print("Suhu adalah", reamur, "Reamur")
+
+# fahrenheit
+fahrenheit = ((9/5) * celcius) + 32
+print("Suhu adalah", fahrenheit, "Fahrenheit")
+
+# kelvin
+kelvin = ((9/5) * celcius) + 32
+print("Suhu adalah", kelvin, "Kelvin")
+
